e7532bf3e074c9309b93c32cab64f0e913340ef8
David Onoh <davidonoh@gmail.com>
2026-02-01 07:56:28 +0100

    Initial Commit

---
+++ TikTok-Style-AppUITests/TikTok_Style_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TikTok_Style_AppUITestsLaunchTests.swift
+//  TikTok-Style-AppUITests
+//
+//  Created by DavidOnoh on 2/1/26.
+//
+
+import XCTest
+
+final class TikTok_Style_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
